ba978d624141bd28c19bcbc4be261c10faff2eab
Rami3L <rami3l@outlook.com>
2020-06-06 11:19:58 +0800

    feat: add skeleton for replace action

---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const format = function (str, col) {
+    col = typeof col === 'object' ? col : Array.prototype.slice.call(arguments, 1);
+
+    return str.replace(/\{\{|\}\}|\{(\w+)\}/g, function (m, n) {
+        if (m == "{{") {
+            return "{";
+        }
+        if (m == "}}") {
+            return "}";
+        }
+        return col[n];
+    });
+};
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    // Get the replaced text based on the template and the items given.
+    const template = core.getInput("template");
+    const items = JSON.parse(core.getInput("items"));
+    const replaced = format(template, items);
+    console.log(`Replaced text: ${replaced}`);
+    core.setOutput("replaced", replaced);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
