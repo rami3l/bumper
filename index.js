faa31f54daf19e9996c65d4607f18f7dd8d0003f
Rami3L <rami3l@outlook.com>
2020-06-07 00:32:04 +0800

    feat: read template file and rules file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs')
 
 const format = function (str, col) {
     col = typeof col === 'object' ? col : Array.prototype.slice.call(arguments, 1);
@@ -20,16 +21,19 @@ try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
 
-    // Get the replaced text based on the template and the items given.
-    const template = core.getInput("template");
-    const items = JSON.parse(core.getInput("items"));
-    const replaced = format(template, items);
+    // Get the replaced text based on the template and the rules given.
+    const template = fs.readFileSync(core.getInput("template"), 'utf8');
+    const rules_str = fs.readFileSync(core.getInput("rules"), 'utf8');
+    const rules = JSON.parse(rules_str);
+    const replaced = format(template, rules);
     // console.log(`Replaced text: ${replaced}`);
     core.setOutput("replaced", replaced);
 
+    fs.writeFileSync(core.getInput("out"), replaced, 'utf8');
+
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
